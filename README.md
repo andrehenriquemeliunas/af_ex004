5d6784e47ba6fc3cf2226c189d823e4307e047eb
Andre Henrique <andremeliunas@hotmail.com>
2022-06-12 10:17:25 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# af_ex004
- Exercício 4 AF
+# Exercício 4 da AF
+Imagem
+
+## Algoritmo do Exercício
+
+    INÍCIO
+
+    VAR
+    i, j, m : inteiros
+
+    PARA i -> 0, i -> 16, i = i + 1, FAÇA
+        ESCREVA('Escreva os elementos da linha [x]' -> i + 1)
+        PARA J = 0, J -> 16, i = i + 1, FAÇA
+            ESCREVA('m[x][y]' -> i, j)
+        FIM PARA
+    FIM PARA
+    PARA i = 0, j -> 16, i = i + 1, FAÇA
+        PARA j = 0, j -> 16, j = j + 1, FAÇA
+            ESCREVA('x' -> m[i][j])
+        FIM PARA
+    FIM PARA
+
+    FIM
+
+## Fluxograma
+<img src="ex_imagens.jpeg" alt="">
--- ex_04.java
+++ ex_04.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 public class ex_04{
     public static void main(String[] args){
-        Scanner ler = new Scanner(System.in);
-        int i, j, m[][] = new int [3][3];
-        for (i = 0; i < 3; i++){
-            System.out.printf("Informe os elementos %da. linha: \n", (i + 1));
-            for (j = 0; j < 3; j++){
-                System.out.printf("m[%d][%d] = ", i, j);
-                m[i][j] = ler.nextInt();
+        Scanner ler = new Scanner(System.in);//Início Scanner
+        int i, j, m[][] = new int [16][16];//Declaração das variáveis e vetor
+        for (i = 0; i <= 16; i++){//Laço de repetição for para perguntar as linhas
+            System.out.printf("Informe os elementos %da. linha: \n", (i + 1));//Solicita ao usuário que informe os elementos da linha correspondente
+            for (j = 0; j <= 16; j++){//Laço de repetição for para adicionar o número da linha nas variáveis i e j
+                System.out.printf("m[%d][%d] = ", i, j);//Exibe a coluna e linha
+                m[i][j] = ler.nextInt();//Leitura do scanner da coluna e linha
             }
-            System.out.printf("\n");
+            System.out.printf("\n");//Print quebrando linha
         }
-        System.out.printf("\n");
-        for (i = 0; i < 3; i++){
-            //System.out.printf("%da. linha: ", (i + 1));
-            for (j = 0; j < 3; j++){
-                System.out.printf("%d ", m[i][j]);
+        System.out.printf("\n");//Print quebrando linha
+        for (i = 0; i <= 16; i++){//Laço de repetição for
+            for (j = 0; j <= 16; j++){//Laço de repetição for
+                System.out.printf("%d ", m[i][j]);//Exibe as linhas no resultado
             }
-            System.out.printf("\n");
+            System.out.printf("\n");//Print quebrando linha
         }
     }
 }
